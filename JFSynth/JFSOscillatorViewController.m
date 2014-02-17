d8be858554ce529dab41d3d157d54f8ecd5e4885
John Forester <forester.john@gmail.com>
2014-02-17 13:33:42 -0500

    oscillator view controller sends control directly to oscillator

--- JFSynth/JFSOscillatorViewController.m
+++ JFSynth/JFSOscillatorViewController.m
@@ -24,7 +24,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    
     self.view.layer.borderColor = [UIColor redColor].CGColor;
     self.view.layer.borderWidth = 1.0;
     
@@ -35,13 +35,18 @@
     
     self.fineSlider.minimumValue = [[synthController minimumValueForParameter:JFSSynthParamOscillator1Fine] floatValue];
     self.fineSlider.maximumValue = [[synthController maximumValueForParameter:JFSSynthParamOscillator1Fine] floatValue];
-
+    
     self.volumeSlider.minimumValue = [[synthController minimumValueForParameter:JFSSynthControllerOscillator1Volume] floatValue];
     self.volumeSlider.maximumValue = [[synthController maximumValueForParameter:JFSSynthControllerOscillator1Volume] floatValue];
     
     self.semitoneSlider.displayType = JFSKnobDisplayTypeInteger;
-    self.semitoneSlider.value = [synthController.oscillators[0] semitones];
-    self.fineSlider.value = [synthController.oscillators[0] fine];
+    self.semitoneSlider.value = [self.oscillator semitones];
+    self.fineSlider.value = [self.oscillator fine];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
 }
 
 - (IBAction)waveTypeControlChanged:(UISegmentedControl *)segmentedControl
@@ -51,7 +56,13 @@
 
 - (IBAction)knobValueChanged:(JFSKnob *)knob
 {
-    [[JFSSynthController sharedController] setValue:knob.value forParameter:knob.tag];
+    if (knob == self.volumeSlider) {
+        [self.oscillator updateVolume:knob.value];
+    } else if (knob == self.semitoneSlider) {
+        [self.oscillator updateSemitone:knob.value];
+    } else if (knob == self.fineSlider) {
+        [self.oscillator updateFine:knob.value];
+    }
 }
 
 @end
